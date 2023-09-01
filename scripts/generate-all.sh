28076996472e5d6436be65f37a6284432057ca46
Gabor Szarnyas <szarnyasg@gmail.com>
2023-09-01 14:10:15 -0700

    Rework generate-all.sh script
    - Reshuffle the order of generation scripts
    - Reinstate the use of the LDBC_SNB_FACTOR_TABLES_DIR environment variable for calling the parameter generator

--- scripts/generate-all.sh
+++ scripts/generate-all.sh
@@ -8,14 +8,16 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
+
 echo "==============================================================================="
 echo "Generating data for SNB Interactive v2"
 echo "-------------------------------------------------------------------------------"
 echo "SF: ${SF}"
-echo "LDBC_SNB_DRIVER_DIR: ${LDBC_SNB_DRIVER_DIR}"
 echo "LDBC_SNB_DATAGEN_DIR: ${LDBC_SNB_DATAGEN_DIR}"
 echo "LDBC_SNB_DATAGEN_MAX_MEM: ${LDBC_SNB_DATAGEN_MAX_MEM}"
-echo "-------------------------------------------------------------------------------"
+echo "LDBC_SNB_DRIVER_DIR: ${LDBC_SNB_DRIVER_DIR}"
+echo "==============================================================================="
+
 
 export LDBC_SNB_IMPLS_DIR=`pwd`
 
@@ -33,43 +35,21 @@ fi
 
 cd ${LDBC_SNB_IMPLS_DIR}
 
+
 echo "==================== Cleanup existing directories ===================="
 mkdir -p update-streams-sf${SF}/
 mkdir -p parameters-sf${SF}/
 rm -rf ${LDBC_SNB_IMPLS_DIR}/update-streams-sf${SF}/*
 rm -rf ${LDBC_SNB_IMPLS_DIR}/parameters-sf${SF}/*
 
-echo "==================== Generate data ===================="
+
+echo "==================== Generate data for update streams and factors ===================="
 cd ${LDBC_SNB_DATAGEN_DIR}
 if ${USE_DATAGEN_DOCKER} && [ -d out-sf${SF} ] && [ ${OSTYPE} != "darwin"* ]; then
     sudo chown -R $(id -u):$(id -g) out-sf${SF}
 fi
 rm -rf out-sf${SF}
 
-echo "-------------------- Generate data for Cypher --------------------"
-if ${USE_DATAGEN_DOCKER} && [ -d out-sf${SF} ] && [ ${OSTYPE} != "darwin"* ]; then
-    sudo chown -R $(id -u):$(id -g) out-sf${SF}
-fi
-rm -rf out-sf${SF}/graphs/parquet/raw
-${DATAGEN_COMMAND} \
-    --mode bi \
-    --format csv \
-    --scale-factor ${SF} \
-    --explode-edges \
-    --epoch-millis \
-    --format-options header=false,quoteAll=true,compression=gzip
-
-echo "-------------------- Generate data for Postgres --------------------"
-if ${USE_DATAGEN_DOCKER} && [ -d out-sf${SF} ] && [ ${OSTYPE} != "darwin"* ]; then
-    sudo chown -R $(id -u):$(id -g) out-sf${SF}
-fi
-rm -rf out-sf${SF}/graphs/parquet/raw
-${DATAGEN_COMMAND} \
-    --mode bi \
-    --format csv \
-    --scale-factor ${SF}
-
-echo "-------------------- Generate data for update streams and factors --------------------"
 if ${USE_DATAGEN_DOCKER} && [ -d out-sf${SF} ] && [ ${OSTYPE} != "darwin"* ]; then
     sudo chown -R $(id -u):$(id -g) out-sf${SF}
 fi
@@ -85,6 +65,33 @@ if ${USE_DATAGEN_DOCKER} && [ ${OSTYPE} != "darwin"* ]; then
 fi
 
 
+echo "==================== Generate parameters ===================="
+cd ${LDBC_SNB_DRIVER_DIR}/paramgen
+export LDBC_SNB_DATA_ROOT_DIRECTORY=${LDBC_SNB_DATAGEN_DIR}/out-sf${SF}/
+export LDBC_SNB_FACTOR_TABLES_DIR=${LDBC_SNB_DATA_ROOT_DIRECTORY}/factors/parquet/raw/composite-merged-fk/
+
+if [[ $SF < 1 ]]
+then
+    export LDBC_THRESHOLD_VALUE_FILE='paramgen_window_values_test.json'
+else
+    export LDBC_THRESHOLD_VALUE_FILE='paramgen_window_values.json'
+fi
+
+# Instead of using the scripts/paramgen.sh script, we call paramgen manually
+# and parameterize it with the ${LDBC_THRESHOLD_VALUE_FILE} file
+# See the rationale for this at https://github.com/ldbc/ldbc_snb_datagen_spark/issues/429
+python3 paramgen.py \
+    --raw_parquet_dir "${LDBC_SNB_DATA_ROOT_DIRECTORY}/graphs/parquet/raw/" \
+    --factor_tables_dir "${LDBC_SNB_FACTOR_TABLES_DIR}/" \
+    --time_bucket_size_in_days 1 \
+    --generate_short_query_parameters True \
+    --threshold_values_path ${LDBC_THRESHOLD_VALUE_FILE}
+
+cd ..
+
+mv parameters/*.parquet ${LDBC_SNB_IMPLS_DIR}/parameters-sf${SF}/
+
+
 echo "==================== Generate update streams ===================="
 cd ${LDBC_SNB_DRIVER_DIR}
 cd scripts
@@ -94,27 +101,29 @@ export LDBC_SNB_DATA_ROOT_DIRECTORY=${LDBC_SNB_DATAGEN_DIR}/out-sf${SF}/
 mv inserts/ ${LDBC_SNB_IMPLS_DIR}/update-streams-sf${SF}/
 mv deletes/ ${LDBC_SNB_IMPLS_DIR}/update-streams-sf${SF}/
 
-echo "==================== Generate parameters ===================="
-cd ${LDBC_SNB_DRIVER_DIR}/paramgen
-export LDBC_SNB_DATA_ROOT_DIRECTORY=${LDBC_SNB_DATAGEN_DIR}/out-sf${SF}/
 
-if [[ $SF < 1 ]]
-then
-  export LDBC_THRESHOLD_VALUE_FILE='paramgen_window_values_test.json'
-else
-  export LDBC_THRESHOLD_VALUE_FILE='paramgen_window_values.json'
+echo "==================== Generate data for Cypher ===================="
+cd ${LDBC_SNB_DATAGEN_DIR}
+if ${USE_DATAGEN_DOCKER} && [ -d out-sf${SF} ] && [ ${OSTYPE} != "darwin"* ]; then
+    sudo chown -R $(id -u):$(id -g) out-sf${SF}
 fi
+rm -rf out-sf${SF}/graphs/parquet/raw
+${DATAGEN_COMMAND} \
+    --mode bi \
+    --format csv \
+    --scale-factor ${SF} \
+    --explode-edges \
+    --epoch-millis \
+    --format-options header=false,quoteAll=true,compression=gzip
 
-# https://github.com/ldbc/ldbc_snb_datagen_spark/issues/429
-# Instead of using the scripts/paramgen.sh script, we call paramgen manually
-# and parameterize it with the ${LDBC_THRESHOLD_VALUE_FILE} file
-python3 paramgen.py \
-  --raw_parquet_dir "${LDBC_SNB_DATA_ROOT_DIRECTORY}/graphs/parquet/raw/" \
-  --factor_tables_dir "${LDBC_SNB_FACTOR_TABLES_DIR}" \
-  --time_bucket_size_in_days 1 \
-  --generate_short_query_parameters True \
-  --threshold_values_path ${LDBC_THRESHOLD_VALUE_FILE}
 
-cd ..
-
-mv parameters/*.parquet ${LDBC_SNB_IMPLS_DIR}/parameters-sf${SF}/
+echo "==================== Generate data for Postgres =================="
+cd ${LDBC_SNB_DATAGEN_DIR}
+if ${USE_DATAGEN_DOCKER} && [ -d out-sf${SF} ] && [ ${OSTYPE} != "darwin"* ]; then
+    sudo chown -R $(id -u):$(id -g) out-sf${SF}
+fi
+rm -rf out-sf${SF}/graphs/parquet/raw
+${DATAGEN_COMMAND} \
+    --mode bi \
+    --format csv \
+    --scale-factor ${SF}
